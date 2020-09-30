40e3867f30296fdcc607594550706c70cfe5df57
George Calin <george.calin@gmail.com>
2020-09-30 19:55:02 +0300

    Understanding Widgets

---
+++ 3-1QtWidgetsDemo/widget.cpp
@@ -0,0 +1,17 @@
+#include "widget.h"
+#include "ui_widget.h"
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+    ui->pushButton->setText("Changed Text");
+
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
